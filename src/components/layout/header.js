cf83be989c89aabb687566b473da6f06217ed6e0
tinluong <tinluong>
2023-10-01 17:05:13 +0700

    header, footer

---
+++ src/components/layout/header.js
@@ -0,0 +1,6 @@
+var btnSearch = document.querySelector('.search-box_btn')
+
+btnSearch.addEventListener('click', function(){
+    this.parentElement.classList.toggle('search-box_open')
+    this.previousElementSibling.focus();
+})
