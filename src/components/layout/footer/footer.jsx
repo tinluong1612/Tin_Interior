61cb6599c16912fc4df77470a28e0faa21163ee5
tinluong <tinluong>
2023-10-03 22:26:38 +0700

    upload

--- src/components/layout/footer/footer.jsx
+++ src/components/layout/footer/footer.jsx
@@ -1,5 +1,95 @@
+
+import './footer.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebookF, faInstagram, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons';
+
+
+
 function Footer() {
-    return <h2>Footer</h2>;
+    return <div>
+        <div className="col-60 footer">
+            <div className="main">
+                <div className="col-20 sub-content">
+                    <div className="logo">
+                        <img src="/src/assets/img/Logo.png" width="30px" height="30px" alt="" />
+                        <div className="name">Interno</div>
+                    </div>
+                    <p className="slogan">
+                        It is a long established fact that a reader will be distracted lookings.
+                    </p>
+                    <div className="social">
+
+                        <a href="" className='icon-social'>
+                            <FontAwesomeIcon icon={faFacebookF} />
+                        </a>
+                        <a href="" className='icon-social'>
+                            <FontAwesomeIcon icon={faTwitter} />
+                        </a>
+                        <a href="" className='icon-social'>
+                            <FontAwesomeIcon icon={faLinkedin} />
+                        </a>
+                        <a href="" className='icon-social'>
+                            <FontAwesomeIcon icon={faInstagram} />
+                        </a>
+                    </div>
+                </div>
+                {/* Pages */}
+                <div className="col-10 page-content">
+                    <h2 className="content">Pages</h2>
+                    <a href="" className="info">
+                        About Us
+                    </a>
+                    <a href="" className="info">
+                        Our Projects
+                    </a>
+                    <a href="" className="info">
+                        Our Team
+                    </a>
+                    <a href="" className="info">
+                        Contact Us
+                    </a>
+                    <a href="" className="info">
+                        Services
+                    </a>
+                </div>
+                {/* Services */}
+                <div className="col-10 page-content">
+                    <h2 className="content">Services</h2>
+                    <a href="" className="info">
+                        Kitchan
+                    </a>
+                    <a href="" className="info">
+                        Living Area
+                    </a>
+                    <a href="" className="info">
+                        Bathroom
+                    </a>
+                    <a href="" className="info">
+                        Dinning Hall
+                    </a>
+                    <a href="" className="info">
+                        Bedroom
+                    </a>
+                </div>
+                {/* Contact */}
+                <div className="col-20 contact">
+                    <h2 className="content">Contact</h2>
+                    <p className="info">55 East Birchwood Ave. Brooklyn, New York 11201</p>
+                    <p className="mgtd-45 info">khanhtin.luong1612@gmail.com</p>
+                    <p className="mgtd-45 info">(+84) 81330 - 0435</p>
+                </div>
+            </div>
+            {/* Coppy Right */}
+            <div className="copyright">
+                <hr className="line" />
+                <p className="info-copyright">
+                    Copyright © Interno | Designed by Victorflow Templates - Powered by
+                    Webflow
+                </p>
+            </div>
+        </div>
+
+    </div>;
 }
 
 export default Footer;
--- src/components/layout/header/header.jsx
+++ src/components/layout/header/header.jsx
@@ -1,4 +1,43 @@
+import './header.css'
+import '../../../index.css'
+// import './index.js'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
+
+
 function Header() {
-    return <h2>Header</h2>;
+    return <div>
+        <div className="col-60 header">
+            <div className="logo">
+                <img src="/src/assets/img/Logo.png" width="40px" height="40px" alt="" />
+                <div className="name">Interno</div>
+            </div>
+            <span className="menubar">
+                <ul className="menubar-item">
+                    <a href="">Home</a>
+                </ul>
+                <ul className="menubar-item">
+                    <a href="">About</a>
+                </ul>
+                <ul className="menubar-item">
+                    <a href="">Service</a>
+                </ul>
+                <ul className="menubar-item">
+                    <a href="">Pricing</a>
+                </ul>
+                <ul className="menubar-item">
+                    <a href="">Blog</a>
+                </ul>
+                <div className="search-box">
+                    <input className="search-box_input" type="text" />
+                    <button className="search-box_btn">
+                    <FontAwesomeIcon icon={faSearch} />
+                    </button>
+                </div>
+            </span>
+        </div>
+
+    </div>;
 }
 export default Header;
